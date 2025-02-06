9824c7de5c891da9969454c14f9dd0a633665fb3
amreeahm <khanamrees@gmail.com>
2025-02-06 15:06:50 -0800

    ME_FE_BUILDOUT_XSTOPWATCH_MODULE_ONE

--- src/App.js
+++ src/App.js
@@ -1,10 +1,45 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [running,setRunning] = useState(false);
+  const [time,setTime] = useState(0);
+
+  useEffect(() => {
+    let intervalID;
+
+    if(running){
+      intervalID = setInterval(()=>{
+        setTime(prev => prev + 1)
+      },1000)
+    }else{
+      clearInterval(intervalID)
+    }
+
+    return () => clearInterval(intervalID);
+  },[running])
+
+  const startStop = () => {
+    setRunning(prev => !prev);
+  }
+
+  const reset = () =>{
+    setRunning(false);
+    setTime(0)
+  }
+
+  const formatTime = (time) =>{
+    const minutes = Math.floor(time/60);
+    const seconds = time%60;
+    return `${minutes}:${seconds<10?"0":""}${seconds}`
+  }
   return (
-    <div className="App">
-      Add
-    </div>
+    <>
+    <h1>Stopwatch</h1>
+    <p>Time: {formatTime(time)}</p>
+    <button onClick={startStop}>{running? "Stop" : "Start"}</button>
+    <button onClick={reset}>Reset</button>
+    </>
   );
 }
 
